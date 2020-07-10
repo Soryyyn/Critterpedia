d9cc03fcbcda07e175f7e3b26cf3247f5a4072f2
Soryyyn <soryn.baechli@gmail.com>
2020-07-10 12:03:45 +0200

    ⚡️ added bad-words filter and post limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,18 @@
 import * as express from "express";
+import axios from "axios";
+
+// express plugins
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as morgan from "morgan";
-const axios = require('axios');
+import * as rateLimit from "express-rate-limit";
 
+// more imports
 import { connection } from "./db";
+import Filter = require("bad-words");
 
 const app = express();
+const filter = new Filter();
 
 // use express plugin for useful information & more
 app.use(morgan("combined")); // better logging
@@ -44,6 +50,29 @@ app.get("/fish/:userid", (req, res) => {
     }
 });
 
+// get all bugs from api
+app.get("/bugs", (req, res) => {
+    axios.get("http://acnhapi.com/v1/bugs")
+        .then((response: any) => {
+            res.send(response.data);
+        })
+        .catch((error: Error) => {
+            throw error;
+        });
+});
+
+// get saved bugs from user_bugs table
+app.get("/bugs/:userid", (req, res) => {
+    try {
+        connection.query(`SELECT * FROM user_bugs WHERE user = "${req.params.userid}"`, (error: Error, results: any) => {
+            if (error) throw error;
+            res.send(Object.values(JSON.parse(JSON.stringify(results))));
+        });
+    } catch (error) {
+        throw error;
+    }
+});
+
 // update or insert change from fishes page
 app.post("/fish", (req, res) => {
     let changes = req.body;
@@ -72,28 +101,11 @@ app.post("/fish", (req, res) => {
     }
 });
 
-// get all bugs from api
-app.get("/bugs", (req, res) => {
-    axios.get("http://acnhapi.com/v1/bugs")
-        .then((response: any) => {
-            res.send(response.data);
-        })
-        .catch((error: Error) => {
-            throw error;
-        });
-});
-
-// get saved bugs from user_bugs table
-app.get("/bugs/:userid", (req, res) => {
-    try {
-        connection.query(`SELECT * FROM user_bugs WHERE user = "${req.params.userid}"`, (error: Error, results: any) => {
-            if (error) throw error;
-            res.send(Object.values(JSON.parse(JSON.stringify(results))));
-        });
-    } catch (error) {
-        throw error;
-    }
-});
+// limit express posts
+app.use(rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 10 // limit each IP to 100 posts
+}));
 
 // update or insert change from bugs page
 app.post("/bugs", (req, res) => {
@@ -126,55 +138,51 @@ app.post("/bugs", (req, res) => {
 // signup user in table
 app.post("/signup", (req, res) => {
 
+    // convert input from form to strings
+    // and filter out bad words
     let user = req.body;
+    user.nickname = filter.clean(user.nickname.toString());
+    user.email = filter.clean(user.email.toString());
+    user.password = filter.clean(user.password.toString());
 
-    // check if password is 6 or more characters
-    // & is a mix of numbers and letters
-    if (this.password.match(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/)) {
+    // test if user with same nickname or email exists in users table
+    // if there isnt send error to client
+    // else create new entry for user in table
+    // and send userid to client for better management
+    try {
+        connection.query(`SELECT * FROM users WHERE nickname = "${user.nickname}" OR email = "${user.email}"`, (error: Error, results: any) => {
+            if (error) throw error;
 
-        // check if email is valid
-        if (this.email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
-
-            // test if user with same nickname or email exists in users table
-            // if there isnt send error to client
-            // else create new entry for user in table
-            // and send userid to client for better management
-            try {
-                connection.query(`SELECT * FROM users WHERE nickname = "${user.nickname}" OR email = "${user.email}"`, (error: Error, results: any) => {
-                    if (error) throw error;
-
-                    if (results.length < 1) {
-                        connection.query(`INSERT INTO users (nickname, email, password) VALUES ("${user.nickname}", "${user.email}", "${user.password}")`, (error: Error) => {
-                            if (error) {
-                                throw error
-                            }
-
-                            connection.query(`SELECT id FROM users WHERE nickname = "${user.nickname}" OR email = "${user.email}"`, (error: Error, results: any) => {
-                                if (error) throw error;
-                                res.send({ userid: results[0].id });
-                            });
-                        })
-                    } else {
-                        res.send("User with same nickname or email is already in use");
+            if (results.length < 1) {
+                connection.query(`INSERT INTO users (nickname, email, password) VALUES ("${user.nickname}", "${user.email}", "${user.password}")`, (error: Error) => {
+                    if (error) {
+                        throw error
                     }
 
-                });
-            } catch (error) {
-                throw error;
+                    connection.query(`SELECT id FROM users WHERE nickname = "${user.nickname}" OR email = "${user.email}"`, (error: Error, results: any) => {
+                        if (error) throw error;
+                        res.send({ userid: results[0].id });
+                    });
+                })
+            } else {
+                res.send("User with same nickname or email is already in use");
             }
 
-        } else {
-            res.send("Email is not valid");
-        }
-    } else {
-        res.send("Password doesn't match the rules");
+        });
+    } catch (error) {
+        throw error;
     }
 
 });
 
 // signin user
 app.post("/signin", (req, res) => {
+
+    // convert input from form to strings
+    // and filter out bad words
     let user = req.body;
+    user.email = filter.clean(user.email.toString());
+    user.password = filter.clean(user.password.toString());
 
     // test if user with same email exists in users table
     // if there isnt send error to client
